d6f762217cf230d797fabe1e5f9b3c90d20fc850
Tim Kluge <timklge@gmail.com>
2025-06-26 22:41:16 +0200

    Only refresh player data field if it is currently visible

--- app/src/main/kotlin/de/timklge/karoospintunes/Extensions.kt
+++ app/src/main/kotlin/de/timklge/karoospintunes/Extensions.kt
@@ -1,12 +1,14 @@
 package de.timklge.karoospintunes
 
 import io.hammerhead.karooext.KarooSystemService
+import io.hammerhead.karooext.models.ActiveRidePage
 import io.hammerhead.karooext.models.OnStreamState
 import io.hammerhead.karooext.models.StreamState
 import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.channels.trySendBlocking
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.callbackFlow
+import kotlinx.coroutines.flow.map
 import kotlinx.serialization.Serializable
 
 fun KarooSystemService.streamDataFlow(dataTypeId: String): Flow<StreamState> {
@@ -17,6 +19,25 @@ fun KarooSystemService.streamDataFlow(dataTypeId: String): Flow<StreamState> {
         awaitClose {
             removeConsumer(listenerId)
         }
+    }
+}
+
+fun KarooSystemService.streamActiveRidePage(): Flow<ActiveRidePage> {
+    return callbackFlow {
+        val listenerId = addConsumer { activeRidePage: ActiveRidePage ->
+            trySendBlocking(activeRidePage)
+        }
+        awaitClose {
+            removeConsumer(listenerId)
+        }
+    }
+}
+
+fun KarooSystemService.streamDatatypeIsVisible(
+    datatype: String,
+): Flow<Boolean> {
+    return streamActiveRidePage().map { page ->
+        page.page.elements.any { it.dataTypeId == datatype }
     }
 }
 
